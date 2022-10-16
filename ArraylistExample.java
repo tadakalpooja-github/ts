c78912281e83b04b395198663abe513e26d53374
tadakalpooja-github <115058291+tadakalpooja-github@users.noreply.github.com>
2022-10-16 14:42:38 +0530

    one

---
+++ ArraylistExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayListexample {
+	
+
+	
+	  public static void main(String[] args){
+
+	    // create ArrayList
+	    ArrayList<String> languages = new ArrayList<>();
+
+	    // Add elements to ArrayList
+	    languages.add("Java");
+	    languages.add("Python");
+	    languages.add("Swift");
+	    System.out.println("ArrayList: " + languages);
+	  }
+	}
+
